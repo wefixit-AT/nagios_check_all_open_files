f544762a03755c4e1153d82cb96b819f89dd5047
Johannes Starosta <J.Starosta@bghw.de>
2020-09-11 08:25:55 +0000

    .UPDATE check_all_open_files.sh Added optional parameter to check all opened files by a certain program

--- check_all_open_files.sh
+++ check_all_open_files.sh
@@ -4,40 +4,64 @@
 #
 # Author: Gregor Binder
 # Mail: office@wefixit.at
+# https://github.com/wefixit-AT/nagios_check_all_open_files
+# Adapted for BGHW by Johannes Starosta <j.starosta@bghw.de>
 
-SUDO=/bin/sudo
-LSOF=/sbin/lsof
+# Bash strict mode, to make debugging easier see http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
+SUDO=$(which sudo)
+LSOF=$(which lsof)
+PGREP=$(which pgrep)
+WC=$(which wc)
+PROGRAM=""
 
 ERROR_CODE=-1
-if [ -z "$1" ] || [ -z "$2" ] || [ $2 -lt $1 ] ; then
-    echo "Usage: $0 warning critical"
+set +u
+if [ -z "$1" ] || [ -z "$2" ] || [ "$2" -lt "$1" ] ; then
+    echo "Usage: $0 warning critical program"
     echo "  warning: int"
     echo "  critical: int and >= warning"
+    echo " program: Optional String with name of program"
     echo "IMPORTANT: sudo must set without password asking to allow lsof"
     exit $ERROR_CODE
 else
     WARNING=$1
     CRITICAL=$2
 fi
+if [ ! -z "$3" ];then
+    PROGRAM="$3"
+fi
+set -u
 
 function checkExitStatus {
-    if [ $1 -ne 0 ]; then
+    if [ "$1" -ne 0 ]; then
         echo "!!! command failure !!! $2"
         exit -1
     fi
 }
-
-LSOF=`$SUDO $LSOF | wc -l`
-
-if [ $LSOF -lt $WARNING ]; then
-    printf "OK $LSOF files open|files=$LSOF;$WARNING;$CRITICAL;0\n"
+if [ -z "$PROGRAM" ];then
+    LSOF=$("$SUDO" "$LSOF" | "$WC" -l)
+else
+    PGREP=$("$PGREP" --full "$3")
+    summe=0
+    for i in $PGREP
+    do
+        tmp=$("$SUDO" "$LSOF" -p "$i" | "$WC" -l)
+        summe=$((summe + tmp))
+    done
+    LSOF=$summe
+fi
+if [ "$LSOF" -lt "$WARNING" ]; then
+    echo "OK $LSOF files open|files=$LSOF;$WARNING;$CRITICAL;0"
     ERROR_CODE=0
 else
-    if [ $LSOF -ge $WARNING ] && [ $LSOF -le $CRITICAL ]; then
-        printf "WARN $LSOF files open|files=$LSOF;$WARNING;$CRITICAL;0\n"
+    if [ "$LSOF" -ge "$WARNING" ] && [ "$LSOF" -le "$CRITICAL" ]; then
+        echo "WARN $LSOF files open|files=$LSOF;$WARNING;$CRITICAL;0"
         ERROR_CODE=1
-    elif [ $LSOF -ge $CRITICAL ]; then
-        printf "CRIT $LSOF files open|files=$LSOF;$WARNING;$CRITICAL;0\n"
+    elif [ "$LSOF" -ge "$CRITICAL" ]; then
+        echo "CRIT $LSOF files open|files=$LSOF;$WARNING;$CRITICAL;0"
         ERROR_CODE=2
   fi
 fi
